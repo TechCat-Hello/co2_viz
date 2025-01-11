b3ed035bf110655c1d66583c3a9d981e2fe2f266
TechCat <a.m.techcat@gmail.com>
2025-01-11 11:43:07 +0900

    Remove unused templatetags folder

--- weatherco2app/templatetags/custom_filters.py
+++
@@ -1,9 +0,0 @@
-# weatherco2app/templatetags/custom_filters.py
-import base64
-from django import template
-
-register = template.Library()
-
-@register.filter
-def b64encode(value):
-    return base64.b64encode(value).decode('utf-8')
